9b3c870ad04caa9dd0b11a08120e1120d0dcafd7
kenmaready <ken@xr3.co>
2021-02-24 16:28:10 -0500

    0.0.3.5 Correction in prior commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-console.log("baseURL:", baseURL);
+console.log("baseURL:", process.env.REACT_APP_API_URL);
 
 const instance = axios.create({
     baseUrl: process.env.REACT_APP_API_URL,
