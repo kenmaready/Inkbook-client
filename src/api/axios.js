9694a340ba8ab56ae0ee3eda849828085e57c572
kenmaready <ken@xr3.co>
2021-02-24 16:34:50 -0500

    v.0.0.3.6 variable correction in axios.create()

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-console.log("baseURL:", process.env.REACT_APP_API_URL);
-
 const instance = axios.create({
-    baseUrl: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default instance;
